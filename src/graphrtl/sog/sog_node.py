fc205cad8b2658c3304b310c7d83ab3a593175b8
Armatage, Joaquin <joaquin.armatage@intel.com>
2025-12-08 21:01:36 -0700

    add an sog node dataclass

---
+++ src/graphrtl/sog/sog_node.py
@@ -0,0 +1,21 @@
+"""Dataclass for a node in a simple operator graph (SOG)."""
+
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class SOGNode:
+    """Dataclass for a node in a simple operator graph (SOG)."""
+
+    name: str
+    type: str
+    bit_width: int = 1
+    fanout: int = 1
+    toggle_rate: float = 0.5
+    transition_time: float = 0.5
+    delay: float = 0.0
+    feat: None = None
+
+    def __repr__(self) -> str:
+        """Represent the SOGNode by its name."""
+        return self.name
